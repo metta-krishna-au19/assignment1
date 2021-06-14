594fc9c90181e7944058c6229c3bfcbfd3f7d80e
metta-krishna-au19 <85180265+metta-krishna-au19@users.noreply.github.com>
2021-06-14 20:01:30 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number:"))
+
+for prime in range (1,num):
+
+    for i in range (2, prime):
+        if prime % i ==o:
+            break
+        else:
+            print(num)
+
